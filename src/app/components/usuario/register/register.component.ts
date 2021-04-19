19b42eb02c6931d4d25fab0237f8ab4ebfb82775
chris <cxchillogalli@engicoders.com>
2021-04-19 16:46:14 -0500

    feat: add register

--- src/app/components/usuario/register/register.component.ts
+++ src/app/components/usuario/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.registerForm = this.formBuilder.group({
+      usuario: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      repeatPassword: ['', Validators.required]
+    })
+  }
 
   ngOnInit() {
+  }
+
+  register() {
+    console.log(this.registerForm.value, 'register');
   }
 
 }
--- src/app/components/usuario/usuario-routing.module.ts
+++ src/app/components/usuario/usuario-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'recuperar-password', component: RecoverPasswordComponent },
+  { path: 'registrar', component: RegisterComponent },
 ];
 
 @NgModule({
