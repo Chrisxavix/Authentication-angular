20b7f250588d4fcd520637af06422174854944c0
chris <cxchillogalli@engicoders.com>
2021-04-20 11:59:02 -0500

    feat: add recover password

--- src/app/components/usuario/recover-password/recover-password.component.ts
+++ src/app/components/usuario/recover-password/recover-password.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ErroresService } from '../../services/errores.service';
 
 @Component({
   selector: 'app-recover-password',
@@ -11,6 +14,9 @@ export class RecoverPasswordComponent implements OnInit {
   recuperarForm: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
+    private angularFireAuth: AngularFireAuth,
+    private router: Router,
+    private erroresService: ErroresService,
   ) {
     this.recuperarForm = this.formBuilder.group({
       usuario: ['', [Validators.required, Validators.email]],
@@ -21,6 +27,15 @@ export class RecoverPasswordComponent implements OnInit {
   }
 
   recuperarClave() {
-    console.log(this.recuperarForm.value, 'recuperar');
+    const usuario = this.recuperarForm.get('usuario').value;
+    /* Envía al correo */
+    this.angularFireAuth.auth.sendPasswordResetEmail(usuario).then(response => {
+      console.log(response, 'clave');
+      this.router.navigate(['/usuario']);
+      this.recuperarForm.reset();
+    }, (error)=> {
+      console.log(this.erroresService.error(error.code), 'Upss.');
+      this.recuperarForm.reset();
+    })
   }
 }
