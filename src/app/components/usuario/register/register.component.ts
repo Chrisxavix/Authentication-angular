caf607ae88e8f1144f9ffdf8505cf003fcd971ec
chris <cxchillogalli@engicoders.com>
2021-04-20 10:37:51 -0500

    feat: add register with firebase

--- src/app/components/usuario/register/register.component.ts
+++ src/app/components/usuario/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { validateEqualPasswords } from 'src/app/util/validateEqualPasswords';
 
 @Component({
@@ -10,8 +12,11 @@ import { validateEqualPasswords } from 'src/app/util/validateEqualPasswords';
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
+  loading: boolean;
   constructor(
     private formBuilder: FormBuilder,
+    private angularFireAuth: AngularFireAuth,
+    private router: Router,
   ) {
     this.registerForm = this.formBuilder.group({
       usuario: ['', [Validators.required, Validators.email]],
@@ -24,7 +29,27 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    console.log(this.registerForm.value, 'register');  
+    this.loading = true;
+    const usuario = this.registerForm.get('usuario').value;
+    const password = this.registerForm.get('password').value;
+    this.angularFireAuth.auth.createUserWithEmailAndPassword(usuario, password).then(response => {
+      console.log(response, 'creado');
+      this.router.navigate(['/usuario']);
+      this.loading = false;
+    }, (error)=> {
+      console.log(this.error(error.code), 'Upss.');
+      this.loading = false;
+    })
   }
 
+  error(error: string): string {
+    switch(error) {
+      case 'auth/email-already-in-use':
+        return 'El correo ya ha sido utilizado.';
+      case 'auth/weak-password':
+        return 'La clave debe de ser más de 6 caracteres.'
+      default: 
+        return 'Error desconocido.';
+    }
+  }
 }
