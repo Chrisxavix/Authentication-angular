d7b4feb1f49132719700245349c53e746850b1a0
chris <cxchillogalli@engicoders.com>
2021-04-29 13:47:52 -0500

    feat(inicio): function delete

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -7,21 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  error = false;
-  pin = '';
   constructor() { }
 
   ngOnInit() {
-  }
-
-  ingresar() {
-    if (this.pin === '') {
-      this.error = true;
-      setTimeout(()=> {
-        this.error = false;
-      }, 3000)
-    } else {
-      console.log(this.pin, 'correct');
-    }
   }
 }
